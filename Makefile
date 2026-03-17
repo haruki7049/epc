c6172320005f9813f6aae033241c85b46d0e10ea
haruki7049 <tontonkirikiri@gmail.com>
2026-03-17 22:13:36 +0900

    Update

--- Makefile
+++ Makefile
@@ -1,5 +1,34 @@
-PROJECT = epc
-PROJECT_DESCRIPTION = Erlang Parser Combinator
-PROJECT_VERSION = 0.1.0
+all: build
 
-include erlang.mk
+# Build
+build:
+	erl -make
+
+b: build
+
+# Clean
+clean:
+	rm -f ebin/*.beam
+
+c: clean
+
+# Formatters by treefmt-nix
+format:
+	treefmt
+
+f: format
+fmt: format
+
+# Dialyzer
+PLT = .epc_dialyzer.plt
+APPS = erts kernel stdlib compiler crypto syntax_tools parsetools
+
+dialyzer: build $(PLT)
+	dialyzer --plt $(PLT) ./ebin --get_warnings
+
+$(PLT):
+	dialyzer --build_plt --output_plt $(PLT) --apps $(APPS)
+
+# eunit
+eunit: build
+	erl -noshell -pa ebin -eval "eunit:test(epc, [verbose])" -s init stop
--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
     flake-compat.url = "github:edolstra/flake-compat";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
@@ -16,13 +15,23 @@
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
       imports = [
         inputs.treefmt-nix.flakeModule
       ];
 
       perSystem =
-        { pkgs, lib, ... }:
+        {
+          pkgs,
+          lib,
+          config,
+          ...
+        }:
         {
           treefmt = {
             projectRootFile = ".git/config";
@@ -36,21 +45,25 @@
             # GitHub Actions
             programs.actionlint.enable = true;
 
+            # Json
+            programs.jsonfmt.enable = true;
+
             # Markdown
             programs.mdformat.enable = true;
+
+            # ShellScript
+            programs.shellcheck.enable = true;
+            programs.shfmt.enable = true;
           };
 
           devShells.default = pkgs.mkShell {
             nativeBuildInputs = [
-              # Runtime
-              pkgs.beam28Packages.erlang
-
-              # LSP
-              pkgs.nil
-
-              # Tools
-              pkgs.p7zip
+              pkgs.beam28Packages.erlang # Erlang VM
+              pkgs.nil # Nix LSP
+              pkgs.erlang-language-platform # Erlang LSP
             ];
+
+            inputsFrom = [ config.treefmt.build.devShell ];
           };
         };
     };
